7382f224d883b32377248dc1337475119732adc6
unknown <kevin13var1gas@gmail.com>
2026-03-09 19:44:23 -0500

    first commit

---
+++ WinAppInterface/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WinAppInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alFondoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.llamarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aUnFormularioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aUnProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alFondoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.alTextoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aLos2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BtnLlamar = new System.Windows.Forms.Button();
+            this.caraFelizToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.caraTristeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.colorToolStripMenuItem1,
+            this.llamarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alFondoToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.colorToolStripMenuItem.Text = "Archivo";
+            // 
+            // alFondoToolStripMenuItem
+            // 
+            this.alFondoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("alFondoToolStripMenuItem.Image")));
+            this.alFondoToolStripMenuItem.Name = "alFondoToolStripMenuItem";
+            this.alFondoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.alFondoToolStripMenuItem.Text = "Salir";
+            this.alFondoToolStripMenuItem.Click += new System.EventHandler(this.alFondoToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem1
+            // 
+            this.colorToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alFondoToolStripMenuItem1,
+            this.alTextoToolStripMenuItem,
+            this.aLos2ToolStripMenuItem});
+            this.colorToolStripMenuItem1.Name = "colorToolStripMenuItem1";
+            this.colorToolStripMenuItem1.Size = new System.Drawing.Size(59, 24);
+            this.colorToolStripMenuItem1.Text = "Color";
+            // 
+            // llamarToolStripMenuItem
+            // 
+            this.llamarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aUnFormularioToolStripMenuItem,
+            this.aUnProyectoToolStripMenuItem});
+            this.llamarToolStripMenuItem.Name = "llamarToolStripMenuItem";
+            this.llamarToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.llamarToolStripMenuItem.Text = "Llamar";
+            // 
+            // aUnFormularioToolStripMenuItem
+            // 
+            this.aUnFormularioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.caraFelizToolStripMenuItem,
+            this.caraTristeToolStripMenuItem});
+            this.aUnFormularioToolStripMenuItem.Name = "aUnFormularioToolStripMenuItem";
+            this.aUnFormularioToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aUnFormularioToolStripMenuItem.Text = "A un formulario";
+            this.aUnFormularioToolStripMenuItem.Click += new System.EventHandler(this.aUnFormularioToolStripMenuItem_Click);
+            // 
+            // aUnProyectoToolStripMenuItem
+            // 
+            this.aUnProyectoToolStripMenuItem.Name = "aUnProyectoToolStripMenuItem";
+            this.aUnProyectoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aUnProyectoToolStripMenuItem.Text = "A un proyecto";
+            // 
+            // alFondoToolStripMenuItem1
+            // 
+            this.alFondoToolStripMenuItem1.Name = "alFondoToolStripMenuItem1";
+            this.alFondoToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.alFondoToolStripMenuItem1.Text = "al fondo";
+            // 
+            // alTextoToolStripMenuItem
+            // 
+            this.alTextoToolStripMenuItem.Name = "alTextoToolStripMenuItem";
+            this.alTextoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.alTextoToolStripMenuItem.Text = "al texto";
+            // 
+            // aLos2ToolStripMenuItem
+            // 
+            this.aLos2ToolStripMenuItem.Name = "aLos2ToolStripMenuItem";
+            this.aLos2ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aLos2ToolStripMenuItem.Text = "a los 2";
+            // 
+            // BtnLlamar
+            // 
+            this.BtnLlamar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLlamar.Location = new System.Drawing.Point(148, 216);
+            this.BtnLlamar.Name = "BtnLlamar";
+            this.BtnLlamar.Size = new System.Drawing.Size(147, 65);
+            this.BtnLlamar.TabIndex = 1;
+            this.BtnLlamar.Text = "Llamar";
+            this.BtnLlamar.UseVisualStyleBackColor = true;
+            this.BtnLlamar.Click += new System.EventHandler(this.BtnLlamar_Click);
+            // 
+            // caraFelizToolStripMenuItem
+            // 
+            this.caraFelizToolStripMenuItem.Name = "caraFelizToolStripMenuItem";
+            this.caraFelizToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.caraFelizToolStripMenuItem.Text = "cara feliz";
+            // 
+            // caraTristeToolStripMenuItem
+            // 
+            this.caraTristeToolStripMenuItem.Name = "caraTristeToolStripMenuItem";
+            this.caraTristeToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.caraTristeToolStripMenuItem.Text = "cara triste";
+            this.caraTristeToolStripMenuItem.Click += new System.EventHandler(this.caraTristeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnLlamar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alFondoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem alFondoToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem alTextoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem llamarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aUnFormularioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aUnProyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aLos2ToolStripMenuItem;
+        private System.Windows.Forms.Button BtnLlamar;
+        private System.Windows.Forms.ToolStripMenuItem caraFelizToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem caraTristeToolStripMenuItem;
+    }
+}
+
---
+++ WinAppInterface/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppInterface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void alFondoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnLlamar_Click(object sender, EventArgs e)
+        {
+            //todo formulario es una clase 
+            // y lo debemos de llamar como una clase 
+
+            FrmcaraFeliz objCf = new FrmcaraFeliz();
+
+            objCf.ShowDialog();
+        }
+
+        private void aUnFormularioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmcaraFeliz objCf = new FrmcaraFeliz();
+
+            objCf.ShowDialog();
+        }
+
+        private void caraTristeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrCaraTriste objCf = new FrCaraTriste();
+
+            objCf.ShowDialog();
+        }
+    }
+}
